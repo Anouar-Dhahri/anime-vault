2a4f9c512299beba42f3443c0dcc060b924d9693
Anouar Dhahri <anouar.dhahri1@gmail.com>
2024-08-07 15:17:39 +0100

    anime-vault

---
+++ app/action.tsx
@@ -0,0 +1,19 @@
+"use server";
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+const MAX_LIMIT = 8;
+
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&&order=popularity`
+  );
+  const data = await response.json();
+  console.log(Array.isArray(data));
+  if (!Array.isArray(data)) {
+    return <h1>No Anime Exist</h1>
+  }
+
+  return data.map((anime: AnimeProp, index: number) => (
+    <AnimeCard key={anime.id} anime={anime} index={index} />
+  ));
+};
